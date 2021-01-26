329b3f34e224add3c4b7606e8c9381b99f6d5956
Cydnie Seelenbinder <cydalexand@gmail.com>
2021-01-26 21:44:57 +0100

    Created show page and show action.

--- app/controllers/tuxes_controller.rb
+++ app/controllers/tuxes_controller.rb
@@ -1,2 +1,10 @@
 class TuxesController < ApplicationController
+  def show
+    # authorize @tux
+    @tux = Tux.find(params[:id])
+  end
+
+  def tux_params
+    params.require(:tux).permit(:description, :price)
+  end
 end
