cdb2dfdd040e2d6135f2ce26052ef2fdbf82be1a
amber lee <amblynlee@gmail.com>
2021-01-28 20:16:17 +0100

    updated some pundit stufff

--- app/controllers/tuxes_controller.rb
+++ app/controllers/tuxes_controller.rb
@@ -1,13 +1,18 @@
 class TuxesController < ApplicationController
+  include Pundit
+  skip_after_action :verify_authorized, only: [:home]
+
+  skip_before_action :authenticate_user!, only: [:home]
 
   def show
     # authorize @tux
     @tux = Tux.find(params[:id])
+    authorize @tux
   end
 
   
   def index
-    @tuxes = Tux.all
+    @tuxes = policy_scope(Tux)
   end
   
   private
