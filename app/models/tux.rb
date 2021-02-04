f52180e44c9d741e819c5286cbcd32557cf0e21a
Cydnie Seelenbinder <cydalexand@gmail.com>
2021-02-04 20:43:30 +0100

    added dependency to tuxes

--- app/models/tux.rb
+++ app/models/tux.rb
@@ -1,6 +1,6 @@
 class Tux < ApplicationRecord
   #has_one_attached :photo For later with Cloudinary
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   has_many :users, through: :bookings
   belongs_to :user
 end
